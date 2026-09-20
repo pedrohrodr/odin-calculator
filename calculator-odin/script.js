88a40c2847f3355e67a13411e5b720b4c67f71c5
Pedro Rodrigues <pedrohenriqrodr@gmail.com>
2026-09-20 12:00:37 -0400

    Basic logic and structure of the calculator

---
+++ calculator-odin/script.js
@@ -0,0 +1,21 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let num1;
+let num2;
+let operator;
+
+const operate = function(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "x":
+            return multiply(num1, num2);
+        case "÷":
+            return num1 % num2 === 0 ? divide(num1, num2) : divide(num1, num2).toFixed(2);
+    }
+}
